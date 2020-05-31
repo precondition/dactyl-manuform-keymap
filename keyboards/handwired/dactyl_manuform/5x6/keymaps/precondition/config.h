3de4127661cf0ab8498a2b4344e207b8f77686cf
precondition <57645186+precondition@users.noreply.github.com>
2020-05-31 12:30:56 +0200

    Reduced tapping term from 250 to 180 because it felt too high for shift.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/config.h
@@ -1,23 +1,4 @@
-/*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
 #pragma once
-
 
 #define USE_SERIAL
 
@@ -26,8 +7,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define EE_HANDS
 // Rows are doubled-up
 
-// Tapping term is ever so slightly increased from the 200ms default to prevent accidental mod taps.
-#define TAPPING_TERM 250
+// Default tapping term is 200
+#define TAPPING_TERM 180
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
