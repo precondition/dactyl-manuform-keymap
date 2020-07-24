33251d9242affa791d2b35cbb5073b99b063069c
precondition <57645186+precondition@users.noreply.github.com>
2020-07-24 19:26:24 +0200

    Added a quasi-normal QWERTY layer for Plover.
    The only notable difference from the base layer of the Dactyl_manuform's
    default keymap is the presence of C, V, M, and N on the thumb keys as
    those are used for steno vowel thumb keys by Plover.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
@@ -17,6 +17,7 @@ enum layer_names {
     _MOUSE,
     _UPPER,
     _LOWER,
+    _PLOVER,
     _ADJUST,
 };
 
@@ -31,6 +32,7 @@ enum layer_names {
 #define SYM_ENT LT(_SYM, KC_ENT)
 #define LW_SPC LT(_LOWER, KC_SPC)
 #define UP_BSPC LT(_UPPER, KC_BSPC)
+#define PLOVER TG(_PLOVER)
 
 // Miscellaneous keyboard shortcuts in direct access
 #define UNDO LCTL(KC_Z)
@@ -790,7 +792,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_BSLASH,ARROW_R,                                   KC_RALT, KC_GRV,
                                         NAV_TAB,KC_SPC,    KC_BSPC, SYM_ENT,
                                    TD(CA_CC_CV), MOUSE,    KC_DEL , KC_APP ,
-                                        KC_LALT,KC_CAPS,    SH_OS  , OSM(MOD_LSFT)
+                                        PLOVER,KC_CAPS,    SH_OS  , OSM(MOD_LSFT)
   ),
 
   [_SYM] = LAYOUT_5x6(
@@ -870,6 +872,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         _______,_______,    _______,_______,
                                         _______,_______,    _______,_______,
                                         _______,_______,    _______,_______
+    ),
+
+    [_PLOVER] = LAYOUT_5x6(
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,    KC_6   ,KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,    KC_Y   ,KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,    KC_H   ,KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,    KC_N   ,KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+                         KC_LBRC,KC_RBRC,                                  KC_PLUS, KC_EQL,
+                                         KC_C,   KC_V,      KC_N   , KC_M,
+                                         KC_TAB, KC_SPC,    KC_BSPC, KC_DEL,
+                                         PLOVER, KC_GRV,   KC_LGUI, KC_LALT
     ),
 
     [_ADJUST] = LAYOUT_5x6(
