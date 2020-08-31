d45722d532848af45e1e2af9aebb4f1f9c852c35
precondition <57645186+precondition@users.noreply.github.com>
2020-08-31 14:23:42 +0200

    Added modtap trigger before the combos.
    As per @sevanteri's recommendations.
    Read the comments in the code for more information.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/combos.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/combos.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "keymap.h"
+#include "action_tapping.h" // necessary for action_tapping_process
 
 enum combo_events {
     UY_PRN,
@@ -120,6 +121,11 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
+    // Process mod-taps before the combo is fired,
+    // this helps making modifier-aware combos, 
+    // like UY_PRN or BSPCN_NOT, more fluid
+    // when I use them with home row mods.
+    action_tapping_process((keyrecord_t){});
     mod_state = get_mods();
     switch(combo_index) {
         case UY_PRN:
