6da5930ebf66e485f8910ef9724e7f5855c10b92
precondition <57645186+precondition@users.noreply.github.com>
2020-09-18 12:34:09 +0200

    Put accented letters on base layer (mostly numrow)

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
@@ -317,11 +317,11 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK_DHM] = LAYOUT_5x6(
-           REDO, UNDO  ,KC_WH_U,KC_WH_D, KC_F4 , KC_F5 ,    KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11,
-         CACCCV, KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,    KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_MINS,
-         KC_ESC, HOME_A, HOME_R, HOME_S, HOME_T, KC_G  ,    KC_M  , HOME_N, HOME_E, HOME_I, HOME_O,KC_QUOT,
-      KC_BSLASH, KC_Z  , KC_X  , KC_C  , HOME_D, KC_V  ,    KC_K  , HOME_H,KC_COMM, TD_DOT,KC_SLSH, KC_GRV,
-                      KC_BSLASH,ARROW_R,                                   KC_RALT, KC_GRV,
+           REDO, UNDO  ,KC_WH_U,C_CDILA, KC_F4 , KC_F5 ,    KC_F6  ,DED_CIR,E_GRAVE,E_ACUTE, KC_F10, KC_F11,
+        A_GRAVE, KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,    KC_J   , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_MINS,
+         KC_ESC, HOME_A, HOME_R, HOME_S, HOME_T, KC_G  ,    KC_M   , HOME_N, HOME_E, HOME_I, HOME_O,KC_QUOT,
+      KC_BSLASH, KC_Z  , KC_X  , KC_C  , HOME_D, KC_V  ,    KC_K   , HOME_H,KC_COMM, TD_DOT,KC_SLSH,E_ACUTE,
+                      KC_BSLASH,C_CDILA,                                    KC_RALT, KC_GRV,
                                         NAV_TAB, KC_SPC,    KC_BSPC,SYM_ENT,
                                          CACCCV,MS_CAPS,    KC_DEL , KC_UP ,
                                          PLOVER,KC_CAPS,    SH_OS  ,KC_DOWN
