599096e40dba6010d8793dda67fcc830597023d7
precondition <57645186+precondition@users.noreply.github.com>
2020-09-18 12:36:08 +0200

    Experimenting with ergo row of numbers.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
@@ -330,7 +330,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SYM] = LAYOUT_5x6(
 
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,    KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_BSPC,
-        KC_DOT , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,    KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+        KC_DOT , KC_7  , KC_5  , KC_3  , KC_1  , KC_9  ,    KC_8   , KC_0  , KC_2  , KC_4  , KC_6  ,KC_MINS,
         KC_TILD,TD_XCLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,    KC_CIRC,KC_AMPR,KC_ASTR,KC_EQL ,KC_PLUS,KC_MINS,
         _______,_______,_______,_______,DED_CIR,_______,    _______,COMPOSE,_______, KC_DOT,_______,_______,
                         _______,_______,                                    GUILL_L,GUILL_R,
