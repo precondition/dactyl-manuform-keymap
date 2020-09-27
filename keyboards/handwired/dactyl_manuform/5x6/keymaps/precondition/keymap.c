f79dbb99501b9ce7b2be72a3953d8cad2e3d3420
precondition <57645186+precondition@users.noreply.github.com>
2020-09-27 17:51:11 +0200

    More term tweaking...
    The special term for I isn't necessary anymore as I use backspace + i
    for "ion" now.

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
@@ -289,12 +289,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
  */
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case HOME_I:
-            // My ring finger tends to linger on the key
-            // This tapping term allows me to type "ion" effortlessly.
-            return TAPPING_TERM + 200;
         case HOME_O:
-            return TAPPING_TERM + 10;
+            return TAPPING_TERM + 20;
         case SYM_ENT:
             // Very low tapping term to make sure I don't hit Enter accidentally.
             return TAPPING_TERM - 65;
