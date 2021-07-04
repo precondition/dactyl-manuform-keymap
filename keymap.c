bf9b22622bbfcad9ffa3ecfea5e83eff6cd2fe48
precondition <57645186+precondition@users.noreply.github.com>
2021-07-04 16:16:49 +0200

    Retain mods when using layer keys for REPEAT

--- keymap.c
+++ keymap.c
@@ -303,6 +303,18 @@ uint16_t last_keycode = KC_NO;
 uint8_t last_modifier = 0;
 void process_repeat_key(uint16_t keycode, const keyrecord_t *record) {
     if (keycode != REPEAT) {
+        // Early return when holding down a pure layer key
+        // to retain modifiers
+        switch (keycode) {
+            case QK_DEF_LAYER ... QK_DEF_LAYER_MAX:
+            case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+            case QK_LAYER_MOD ... QK_LAYER_MOD_MAX:
+            case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX:
+            case QK_TOGGLE_LAYER ... QK_TOGGLE_LAYER_MAX:
+            case QK_TO ... QK_TO_MAX:
+            case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
+                return;
+        }
         last_modifier = oneshot_mod_state > mod_state ? oneshot_mod_state : mod_state;
         switch (keycode) {
             case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
