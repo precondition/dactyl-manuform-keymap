bda23c861e94e8bd750b811c382b2786802bd49e
precondition <57645186+precondition@users.noreply.github.com>
2022-08-01 21:15:03 +0200

    Use KEYLOC_COMBO (aka 254) instead of "NA" for row,col of combos

--- combos.c
+++ combos.c
@@ -140,14 +140,15 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
     uint8_t idx = 0;
     uint16_t combo_keycode;
     while ((combo_keycode = pgm_read_word(&combo->keys[idx])) != COMBO_END) {
-        uprintf("0x%04X\tNA\tNA\t0x%X\t%u\t0x%02X\t0x%02X\t0\n",
+        uprintf("0x%04X\t%u\t%u\t0x%X\t%u\t0x%02X\t0x%02X\t0\n",
             combo_keycode,
-            /* <missing row information> */
-            /* <missing column information> */
+            KEYLOC_COMBO,
+            KEYLOC_COMBO,
             layer_state|default_layer_state,
             pressed,
             get_mods(),
             get_oneshot_mods()
+            /* tap_count==0 */
         );
         idx++;
     }
